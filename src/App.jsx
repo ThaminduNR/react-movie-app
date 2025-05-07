aa59d78b5720373894784bebd7e38dc1f1afdbc4
ThaminduNR <thaminduniwantha777@gmail.com>
2025-05-07 20:12:02 +0530

    add assets and header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { useState } from 'react';
+import Search from './components/Search';
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <h1 class="text-3xl font-bold underline text-red-300">Hello world!</h1>
+    <main>
+      <img
+        src="./public/assets/hero-bg.png"
+        className="pattern"
+        alt="hero image"
+      />
+      <div className="wrapper">
+        <header>
+          <img src="./public/assets/hero.png" alt="hero image" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h3 className="text-amber-400 text-2xl">{searchTerm}</h3>
+      </div>
+    </main>
   );
 }
 
